83fb446121a3bf5ad42b5b12410024c5eb68bf45
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2025-01-20 21:34:50 -0500

    started crud functions

--- go.mod
+++ go.mod
@@ -8,8 +8,22 @@ require (
 )
 
 require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+)
+
+require (
+	github.com/gofiber/fiber/v2 v2.52.6
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson" //binary json
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -26,10 +28,10 @@ type Employee struct {
 
 var mg MongoInstance
 
-func main() {
+func Connect() error {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	mongoURI := os.Getenv("MONGO_URI")
@@ -38,16 +40,42 @@ func main() {
 	clientOptions := options.Client().ApplyURI(mongoURI)
 	client, err := mongo.NewClient(clientOptions)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	err = client.Connect(context.TODO())
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	db := client.Database(dbName)
 	mg.DB = db
 	mg.client = client
 	log.Println("Connected to Mongo!")
+	return nil
+}
+func GetEmployees(c *fiber.Ctx) error {
+	var employees []Employee
+	collection := mg.DB.Collection("employees")
+	filter := bson.D{}
+	cur, err := collection.Find(context.Background(), filter)
+	if err != nil {
+		return err
+	}
+	err = cur.All(context.Background(), &employees)
+	if err != nil {
+		return err
+	}
+	return c.JSON(employees)
+}
 
+func main() {
+	if err := Connect(); err != nil {
+		log.Fatal(err)
+	}
+	app := fiber.New()
+	app.Get("/employees", GetEmployees)
+	// app.Get("/employee/:id", GetEmployee)
+	// app.Post("/employees", PostEmployee)
+	// app.Put("/employee/:id", PutEmployee)
+	// app.Delete("/employee/:id", DeleteEmployee)
 }
