eef422bbafbd77c8afd8100c8cecacd1197f839c
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2025-01-30 21:42:20 -0500

    test for update employee passing

--- main/main_test.go
+++ main/main_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/torcuata22/hrms-mongo/database"
 	model "github.com/torcuata22/hrms-mongo/models"
 	"github.com/torcuata22/hrms-mongo/routes"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func initdatabase(t *testing.T) {
@@ -28,18 +28,18 @@ func setupapp() *fiber.App {
 	return app
 }
 
-func insertTestEmployee(t *testing.T, employee model.Employee) string {
-	// Insert the employee into the database
-	inserted, err := database.Mg.DB.Collection("employees").InsertOne(context.TODO(), employee)
-	if err != nil {
-		t.Fatalf("Error inserting test employee: %v", err)
-	}
+// func insertTestEmployee(t *testing.T, employee model.Employee) string {
+// 	// Insert the employee into the database
+// 	inserted, err := database.Mg.DB.Collection("employees").InsertOne(context.TODO(), employee)
+// 	if err != nil {
+// 		t.Fatalf("Error inserting test employee: %v", err)
+// 	}
 
-	// Convert the inserted ID from ObjectID to string
-	insertedID := inserted.InsertedID.(primitive.ObjectID).Hex() // .Hex() converts ObjectID to a string
+// Convert the inserted ID from ObjectID to string
+// insertedID := inserted.InsertedID.(primitive.ObjectID).Hex() // .Hex() converts ObjectID to a string
 
-	return insertedID
-}
+// 	return insertedID
+// }
 
 func makeRequest(t *testing.T, app *fiber.App, method, url string) *http.Response {
 	//request
@@ -167,5 +167,92 @@ func TestCreateEmployee(t *testing.T) {
 	}
 	if createdEmployee.Age != testEmployee.Age {
 		t.Errorf("Expected employee age %d, got %d", testEmployee.Age, createdEmployee.Age)
+	}
+}
+
+func TestUpdateEmployee(t *testing.T) {
+	//initialize db
+	initdatabase(t)
+	// Create Fiber app and setup routes
+	app := setupapp()
+
+	//create test employee
+	testEmployee := model.Employee{
+		ID:     "ee5c432f-ba8a-4b69-b512-09d64e18b788",
+		Name:   "John Doe",
+		Salary: 50000.0,
+		Age:    30,
+	}
+	//convert to JSON
+	jsonEmployee, err := json.Marshal(testEmployee)
+	if err != nil {
+		t.Fatalf("Error marshalling test employee: %v", err)
+	}
+
+	//build the request
+	req, err := http.NewRequest("GET", "/employee/"+testEmployee.ID, bytes.NewBuffer(jsonEmployee))
+	if err != nil {
+		t.Fatalf("Error making test request: %v", err)
+	}
+
+	//set the content type//set headers:
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatalf("Error making test request: %v", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+
+	//update the employee information
+	testEmployee.Name = "Jane Doe"
+	testEmployee.Salary = 60000.0
+	testEmployee.Age = 30
+
+	//convert to JSON
+	jsonEmployee, err = json.Marshal(testEmployee)
+	if err != nil {
+		t.Fatalf("Error marshalling test employee: %v", err)
+	}
+
+	//build the request
+	req, err = http.NewRequest("PUT", "/employee/"+testEmployee.ID, bytes.NewBuffer(jsonEmployee))
+	if err != nil {
+		t.Fatalf("Error making test request: %v", err)
+	}
+
+	//set the content type//set headers:
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err = app.Test(req)
+	if err != nil {
+		t.Fatalf("Error making test request: %v", err)
+	}
+
+	if resp.StatusCode != http.StatusNoContent {
+		t.Errorf("Expected status code %d, got %d", http.StatusNoContent, resp.StatusCode)
+	}
+
+	//retrieve updated employee from the database
+	var updatedEmployee model.Employee
+	collection := database.Mg.DB.Collection("employees")
+	filter := bson.M{"id": testEmployee.ID}
+	err = collection.FindOne(context.Background(), filter).Decode(&updatedEmployee)
+	if err != nil {
+		t.Fatalf("Error retrieving updated employee: %v", err)
+	}
+
+	// Verify that the employee's data was updated correctly
+	if updatedEmployee.Name != "Jane Doe" {
+		t.Errorf("Expected employee name %s, got %s", "Jane Doe", updatedEmployee.Name)
+	}
+	if updatedEmployee.Salary != 60000.0 {
+		t.Errorf("Expected employee salary %f, got %f", 60000.0, updatedEmployee.Salary)
+	}
+	if updatedEmployee.Age != 30 {
+		t.Errorf("Expected employee age %d, got %d", 30, updatedEmployee.Age)
 	}
 }
